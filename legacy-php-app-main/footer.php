aae791d33b7908bc038ce943d764db3405ffcb66
rizza99 <44263376+rizza99@users.noreply.github.com>
2024-01-19 11:12:02 +0100

    import opdracht

---
+++ legacy-php-app-main/footer.php
@@ -0,0 +1,7 @@
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2021 Copyright:
+    <a class="text-dark" href="https://mickael-andrieu.com/">Mickaël Andrieu</a>
+  </div>
+  <?php include_once($rootPath.'/viewed_pages.php'); ?>
+</footer>
